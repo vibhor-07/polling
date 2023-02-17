294890d53443fc74f42bc8350ea5e0e11e3367d3
vibhor <vats.vibhor.07@gmail.com>
2023-02-17 11:22:39 +0530

    Changed the timezone

--- voting/voting/settings.py
+++ voting/voting/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # user defined
+    'votes',
 ]
 
 MIDDLEWARE = [
@@ -105,9 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE =  'Asia/Kolkata'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
