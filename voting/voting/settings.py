5d535c26b31541e9f0b48aa2c4953cbbf881e8c8
vibhor <vats.vibhor.07@gmail.com>
2023-02-17 17:43:28 +0530

    added home page

--- voting/voting/settings.py
+++ voting/voting/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # user defined
     'votes',
+    'page',
 ]
 
 MIDDLEWARE = [
@@ -102,8 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
--- voting/voting/urls.py
+++ voting/voting/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('votes/', include('votes.urls')),
+    path('', include('page.urls')),
 ]
